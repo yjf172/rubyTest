055d30d7f4021e94a5d961c7e1aa2abe08660686
yangjinfeng <15884576209@163.com>
2017-06-19 15:06:57 +0800

    test

--- app/views/welcome/test.rb
+++ app/views/welcome/test.rb
@@ -6,3 +6,18 @@ puts "hi, #{name}!"
 # => hi, jim
 
 puts "hi, "+ name
+
+class Apple
+    #类方法
+    def Apple.name
+        'apple'
+    end
+    #实例方法
+    def color
+            'red'
+        end
+end
+
+Apple.new.color
+Apple.name
+Apple.new.name
