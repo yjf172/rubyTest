47e1dfd4c90f34f0e1c05d14f468992368e82480
yangjinfeng <15884576209@163.com>
2017-07-28 15:41:11 +0800

    test

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,10 @@ class ArticlesController < ApplicationController
     def new
     end
     def create
-        render plain: params[:article].inspect
+        @article = Article.new(params[:article])
+
+              @article.save
+              redirect_to @article
     end
+
 end
